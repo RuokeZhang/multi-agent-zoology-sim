bd791b3f99f4b50b19a2eab7b1cf6f4ab6d30a31
Ruoke Zhang <122314742+RuokeZhang@users.noreply.github.com>
2025-12-10 01:47:08 -0800

    chore: scaffold starter project

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+```jsx
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  // TODO: add layout, session context, and theme.
+  return <Component {...pageProps} />;
+}
+```
